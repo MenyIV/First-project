f27271fd68695ac66eb6b2d821c3fb2cec43aa48
MenyIV <jan.suchy.cz@gmail.com>
2015-03-06 13:54:48 +0100

    Create termostat.ino

---
+++ termostat.ino
@@ -0,0 +1,92 @@
+/*
+  Autor: MenyIV
+  Web: 
+  G+:
+*/
+
+//definice knihoven a komplexních proměnných
+#include <LiquidCrystal.h>                        //knihovna LCD
+#include <dht11.h>                                //knihovna čidla DHT11
+#define resistance 1                            //hystereze
+/* DOPLNIT   JAK  JE P5IPOJEN měl bych to mít v programu s počítadlem času tak jsem tam přidal tlačítko na zhaínání
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+ */
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);            // zapojení LCD JHD – 162A
+boolean rel1;                                     //stav RELE   rel1= chlazení; rel2=topení
+boolean rel2;
+unsigned char settemp;                            //proměnná pro nastavenou teplotu
+unsigned char gettemp1;                           //teplota 1/VENKU+vlhkost 2/UVNITR 3/PONORAK  
+char gethum1;
+unsigned char gettemp2;                              
+unsigned char gettemp3;                                  
+
+
+float set=20.0;                                   //proměnné
+int n=0;
+
+void setup() {
+  lcd.begin(16, 2);
+  
+  pinMode(7,OUTPUT); //rel1   - PIN7
+  pinMode(8,OUTPUT); //rel2   - PIN8
+  
+//CIDLA 1-DHT11  2-DS18B20  3-DS18B20 waterproof
+  pinMode(A0,INPUT); // hum senzor
+  pinMode(A1,INPUT); // gettemp1
+  pinMode(A2,INPUT); // gettemp2
+  pinMode(A3,INPUT); // gettemp3
+  
+//VSTUP TLACITKO KY-040
+  pinMode(12,INPUT); // směr +
+  pinMode(11,INPUT); // směr -
+  pinMode(10,INPUT); //SW tlačítko  
+
+//PODSVICENI LCD
+  pinMode(9,INPUT); //TLACITKO
+  
+}
+
+
+
+void loop() {                         //programová smyčka
+
+if digitaRead(     //pokud je tlačítko na podsvícení on tak on jinak off
+
+if digitalRead(10,HIGH) 
+ {
+ }                 //no při kliku na SW nakopni smyčku ve které poběží nastavení. Ukončení a uložení hodnot opět klikem. 
+                    // pokud přeteče tak spadne zpět do smyčky bez uložení hodnot A settime na 30-60s 
+
+
+if (millis() > (1000 + lastJob1))
+   {
+    // zde bude kód programu, který se v mém případě
+    // vykoná každou 1 vteřinu (1000 ms)
+    // no takže zde bude celý program který načte čidla vypíše na display a vrazíto do relátek  hej
+     
+   
+   
+    // když je digitální pin 0 tak na něj zapiš 1
+    if (digitalRead(LED1) == 0) digitalWrite(LED1, HIGH);
+    // jinak zapiš 0
+    else digitalWrite(LED1, LOW);
+   
+   
+   
+
+
+
+}
+
+// výpis na display   -- nutné odzkoušet a porovnat aby to kněčemu vypadalo
+
+ {}
